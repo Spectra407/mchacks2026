42e40054324075f069f4915d13787e48899a660c
DavidBotros05 <david.botros@mail.mcgill.ca>
2026-01-17 20:35:17 -0500

    Health Bar created with dying option

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,34 +3,54 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Diagnostics;
 
 
 public class GameController : MonoBehaviour
 {
-    int progressAmount;
-    public Slider progressSlider;
+    float HealthAmount;
+    public Slider HealthSlider;
+    bool Health = true;
+    public float healthDrainPerSecond = 1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        HealthSlider.value = 0;
-        HealthAmount = 0;
+        HealthSlider.value = 100;
+        HealthAmount = 100;
         Fish.OnFishCollect += IncreaseHealthAmount;
         
     }
 
     void IncreaseHealthAmount(int amount)
     {
-        HealthAmount += amount;
+        HealthAmount = Mathf.Min(HealthAmount + amount, 100f);
         HealthSlider.value = HealthAmount;
 
-        if (HealthAmount <= 0)
+        if (HealthAmount >= 100f)
         {
-            Debug.Log("You Died lol!!");
+            HealthSlider.value = 100f;           
+            UnityEngine.Debug.Log("Next Level");
+        }
+    }
+
+    void DecreaseHealthAmount()
+    {
+        if (HealthAmount <= 0f && Health)
+        {
+            HealthAmount = 0f;
+            UnityEngine.Debug.Log("You Died lol!!!");
+            Health = false;
+        }
+        else
+        {
+            HealthAmount -= Time.deltaTime * healthDrainPerSecond;
+            HealthSlider.value = HealthAmount;
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        DecreaseHealthAmount();
     }
 }
